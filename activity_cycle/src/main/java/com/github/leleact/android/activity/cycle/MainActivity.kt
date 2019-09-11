86b430cae74ac43126d4e4d3021bf30d5fc43efb
leleact <leleact@gmail.com>
2019-09-11 10:43:15 +0800

    Activity life cycle

--- activity_cycle/src/main/java/com/github/leleact/android/activity/cycle/MainActivity.kt
+++ activity_cycle/src/main/java/com/github/leleact/android/activity/cycle/MainActivity.kt
@@ -2,11 +2,45 @@ package com.github.leleact.android.activity.cycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(tag, "onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "onStart()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "onRestart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag, "onDestroy()")
     }
 }
