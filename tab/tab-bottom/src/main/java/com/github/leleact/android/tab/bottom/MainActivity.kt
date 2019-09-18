f1e40038d6bafd9f76cc9b79607924ff258d00c1
leleact <leleact@gmail.com>
2019-09-18 14:02:31 +0800

    refactor project structure

---
+++ tab/tab-bottom/src/main/java/com/github/leleact/android/tab/bottom/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.leleact.android.tab.bottom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bottom_tabs_layout)
+    }
+}
