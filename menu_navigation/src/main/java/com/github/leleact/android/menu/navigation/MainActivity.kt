91e37a74e66a41bf1463fbe33b63733ea8bc0761
leleact <leleact@gmail.com>
2023-07-01 20:50:06 +0800

    chore(menu_navigation): disable key event
    disable key event

--- menu_navigation/src/main/java/com/github/leleact/android/menu/navigation/MainActivity.kt
+++ menu_navigation/src/main/java/com/github/leleact/android/menu/navigation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.github.leleact.android.menu.navigation
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -19,12 +20,23 @@ class MainActivity : AppCompatActivity() {
         actionBar?.setDisplayHomeAsUpEnabled(true)
         val drawer = findViewById<DrawerLayout>(R.id.drawer)
         mDrawerToggle = ActionBarDrawerToggle(
-            this,
-            drawer,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
+            this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
         mDrawerToggle!!.syncState()
         drawer.addDrawerListener(mDrawerToggle!!)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return true;
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
