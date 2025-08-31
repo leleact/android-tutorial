1701c26808d69033ba4ab0b6d97a65d2f7569b3e
leleact <leleact@gmail.com>
2025-08-31 11:14:11 +0800

    chore(*): update android tools
    update android tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.android.build.api.dsl.ApplicationExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("com.android.application") version "8.12.1" apply false
+    id("com.android.application") version "8.12.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
